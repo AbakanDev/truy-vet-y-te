622e229fb3c31d58ff5722c3679d1eda60dfed17
Manh <manh.hoang@dptsolution.net>
2026-05-03 21:49:26 +0700

    Login to register

--- app/src/main/java/com/example/truyvetyte/LoginActivity.kt
+++ app/src/main/java/com/example/truyvetyte/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.truyvetyte
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.login_screen)
+
+        val btnGo = findViewById<Button>(/* id = */ R.id.btn_register)
+        btnGo.setOnClickListener {
+            // Chuyển từ trang hiện tại (this) sang FriendActivity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/truyvetyte/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.truyvetyte
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.sign_in_screen)
+    }
+}
